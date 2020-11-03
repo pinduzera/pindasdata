d78e1d3cfab77a2150249704b7d9b625c0763f02
Eduardo Hellas <ehellas@gmail.com>
2020-11-02 22:22:38 -0300

    overhaul

--- blogdown.R
+++ blogdown.R
@@ -1,7 +1,13 @@
+library("blogdown")
 blogdown::new_site(theme = "g1eny0ung/hugo-theme-dream")
 #blogdown::install_theme("g1eny0ung/hugo-theme-dream")
 blogdown::serve_site()
 blogdown::stop_server()
-blogdown::new_post(title = 'Container Brilhante na AWS (Shiny Docker na AWS)', ext = '.Rmd', subdir = '//post/ShinyAWS',
+blogdown::new_post(title = 'Container Brilhante na AWS (Shiny Docker na AWS)',
+                   ext = ".Rmd",
+                   subdir = 'post/ShinyAWS',
                    author = 'Eduardo Ochetski Hellas', 
                    tags = c('r', 'api','shiny', 'docker', 'aws')) #fazer novo post
+new_post()
+
+blogdown:::new_post_addin()
---
+++ content/about/me.md
@@ -0,0 +1,14 @@
+---
+title: Sobre mim
+---
+Olá, me chamo Eduardo, e estou aqui para compartilhar insights e análises de
+dados. Sou formado em estatística pela Universidade de Brasília (UnB) e
+utilizo o R como principal linguagem de programação para fazer as
+abordagens analíticas.
+
+Como hobby sou apaixonado por jogos de tabuleiro com coca-cola e
+transformar café em análise.
+
+[Curriculum - Pt](/CurriculumVitae_pt.pdf) 
+
+[Curriculum - En](/CurriculumVitae.pdf)
