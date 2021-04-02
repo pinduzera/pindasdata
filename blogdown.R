c5433805babc291e92e051611a9a7a89e2522824
Eduardo Hellas <ehellas@gmail.com>
2021-04-02 01:11:42 -0300

    update cvs

--- blogdown.R
+++ blogdown.R
@@ -3,11 +3,11 @@ blogdown::new_site(theme = "g1eny0ung/hugo-theme-dream")
 #blogdown::install_theme("g1eny0ung/hugo-theme-dream")
 blogdown::serve_site()
 blogdown::stop_server()
-blogdown::new_post(title = 'Container Brilhante na AWS (Shiny Docker na AWS)',
+blogdown::new_post(title = 'Container Brilhante no Azure',
                    ext = ".Rmd",
-                   subdir = 'post/ShinyAWS',
+                   subdir = 'post/ShinyAzure',
                    author = 'Eduardo Ochetski Hellas', 
-                   tags = c('r', 'api','shiny', 'docker', 'aws')) #fazer novo post
+                   tags = c('r', 'api','shiny', 'docker', 'azure')) #fazer novo post
 new_post()
 
 blogdown:::new_post_addin()
